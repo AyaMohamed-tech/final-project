fb148ffadd55c37b16696280ae68f1135028a2b4
islam-abdelkader <islamabdelkader22@gmail.com>
2021-08-07 20:10:33 +0200

    finishing checkout

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
@@ -8,80 +9,104 @@ use Illuminate\Http\Request;
 use App\Slider;
 use App\Product;
 use App\Category;
-// use Session;
 use App\Cart;
 use App\Client;
+use Stripe\Charge;
+use Stripe\Stripe;
+
+// use Session;
 
 
 class ClientController extends Controller
 {
-    public function home(){
+    public function home()
+    {
         //===== get all products in Product model =========
         $products = Product::get();
         //===== get all sliders in Slider model =========
         $sliders = Slider::get();
-        return view('client.home')->with('sliders',$sliders)->with('products',$products);
+        return view('client.home')->with('sliders', $sliders)->with('products', $products);
     }
-    public function cart(){
-        if(!Session::has('cart')){
+    public function cart()
+    {
+        if (!Session::has('cart')) {
             return view('client.cart');
         }
 
-        $oldCart = Session::has('cart')? Session::get('cart'):null;
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
         $cart = new Cart($oldCart);
-        return view('client.cart',['products' => $cart->items]);
-        
+        return view('client.cart', ['products' => $cart->items]);
     }
 
-    public function updateqty(Request $request){
+    public function updateqty(Request $request)
+    {
 
-                //print('the product id is '.$request->id.' And the product qty is '.$request->quantity);
-                $oldCart = Session::has('cart')? Session::get('cart'):null;
-                $cart = new Cart($oldCart);
-                $cart->updateQty($request->id, $request->quantity);
-                Session::put('cart', $cart);
-        
-                //dd(Session::get('cart'));
-                return redirect('/cart');
+        //print('the product id is '.$request->id.' And the product qty is '.$request->quantity);
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        $cart->updateQty($request->id, $request->quantity);
+        Session::put('cart', $cart);
 
+        //dd(Session::get('cart'));
+        return redirect('/cart');
     }
 
-    public function removeitem($id){
+    public function removeitem($id)
+    {
 
-        $oldCart = Session::has('cart')? Session::get('cart'):null;
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
         $cart = new Cart($oldCart);
         $cart->removeItem($id);
-       
-        if(count($cart->items) > 0){
+
+        if (count($cart->items) > 0) {
             Session::put('cart', $cart);
-        }
-        else{
+        } else {
             Session::forget('cart');
         }
 
         //dd(Session::get('cart'));
         return redirect('/cart');
-
     }
-    public function shop(){
+    public function shop()
+    {
         //===== get all categories in Category model =========
         $categories = Category::get();
         //===== get all products in Product model =========
         $products = Product::get();
-        return view('client.shop')->with('products',$products)->with('categories',$categories);
+        return view('client.shop')->with('products', $products)->with('categories', $categories);
     }
-    public function checkout(){
-        if(!Session::has('cart')){
-            return redirect ('/cart');
+    public function checkout()
+    {
+        if (!Session::has('cart')) {
+            return redirect('/cart');
         }
         return view('client.checkout');
     }
 
-    public function postcheckout(){
-        if(!Session::has('cart')){
-            return redirect ('/cart');
+    public function postcheckout(Request $request)
+    {
+        if (!Session::has('cart')) {
+            return redirect('/cart');
         }
-        return view('client.checkout');
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $cart = new Cart($oldCart);
+        Stripe::setApiKey('sk_test_51JLs9OEITolXhvOBPfGxQWcSZ6sBfD9RRhpCB3o4VWmwespXvXSpCiZgZmtztB4u3qTSfKg3H9YbDcQo4c8bxsrK00ZglvFGqc');
+
+        try {
+
+            $charge = Charge::create(array(
+                "amount" => $cart->totalPrice * 100,
+                "currency" => "usd",
+                "source" => $request->input('stripeToken'), // obtainded with Stripe.js
+                "description" => "Test Charge"
+            ));
+        } catch (\Exception $e) {
+            Session::put('error', $e->getMessage());
+            return redirect('/checkout');
+        }
+
+        Session::forget('cart');
+        return redirect('/cart')->with('success', 'Purchase accomplished successfully !');
     }
 
 
@@ -89,66 +114,59 @@ class ClientController extends Controller
 
 
 
-    public function login(){
+    public function login()
+    {
         return view('client.login');
     }
-    public function signup(){
+    public function signup()
+    {
         return view('client.signup');
     }
-    public function createaccount(Request $request){
+    public function createaccount(Request $request)
+    {
 
-        $this->validate($request,['email' => 'email|required|unique:clients' ,
-                                  'password' => 'required|min:4' ]);
-        $client=new Client();
-        $client->email=$request->input('email');
-        $client->password=bcrypt($request->input('password'));  //hash password
-        
+        $this->validate($request, [
+            'email' => 'email|required|unique:clients',
+            'password' => 'required|min:4'
+        ]);
+        $client = new Client();
+        $client->email = $request->input('email');
+        $client->password = bcrypt($request->input('password'));  //hash password
+
         $client->save();
 
-    //    return back()->with('status' , 'Your account has been created successfully');
-       return redirect('/login');
-      
-      }
-      
+        //    return back()->with('status' , 'Your account has been created successfully');
+        return redirect('/login');
+    }
 
-      
 
-        public function accsesaccount(Request $request){
 
-          $this->validate($request,['email' => 'email|required' ,
-                                    'password' => 'required' ]);
-          $client=Client::where('email' ,$request->input('email'))->first(); //email check
-          if($client){
-              if(Hash::check($request->input('password'),$client->password)){
-                 Session::put('client',$client);
-                 return redirect('/shop');
-                 //return back()->with('status','Your Email Is ' .Session::get('client')->email);
 
-              }else{
-                return back()->with('error','Wrong Password or Email !');
+    public function accsesaccount(Request $request)
+    {
 
-              }
+        $this->validate($request, [
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+        $client = Client::where('email', $request->input('email'))->first(); //email check
+        if ($client) {
+            if (Hash::check($request->input('password'), $client->password)) {
+                Session::put('client', $client);
+                return redirect('/shop');
+                //return back()->with('status','Your Email Is ' .Session::get('client')->email);
 
-          }else{
-            return back()->with('error','You Do not Have account');
+            } else {
+                return back()->with('error', 'Wrong Password or Email !');
+            }
+        } else {
+            return back()->with('error', 'You Do not Have account');
+        }
+    }
 
-          }
-      }
-      
-      public function logout(){
+    public function logout()
+    {
         Session::forget('client');
         return back();
-      }
-
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -1,182 +1,188 @@
 @extends('layouts.app')
 
 @section('title')
-  Checkout
+    Checkout
 @endsection
 
 @section('content')
 
     <div class="hero-wrap hero-bread" style="background-image: url('frontend/images/bg_1.jpg');">
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Checkout</span></p>
-            <h1 class="mb-0 bread">Checkout</h1>
-          </div>
+        <div class="container">
+            <div class="row no-gutters slider-text align-items-center justify-content-center">
+                <div class="col-md-9 ftco-animate text-center">
+                    <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Checkout</span></p>
+                    <h1 class="mb-0 bread">Checkout</h1>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
 
     <section class="ftco-section">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xl-7 ftco-animate">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-7 ftco-animate">
 
-						{!!Form::open(['action' => 'ClientController@postcheckout','method' => 'POST'
-							, 'class' = 'billing' ,'id'-> 'checkout-form' ])!!}
-								{{csrf_field()}}
+                    {!! Form::open(['action' => 'ClientController@postcheckout', 'method' => 'POST', 'class' => 'billing', 'id' => 'checkout-form']) !!}
+                    {{ csrf_field() }}
+                    @if (Session::has('error'))
+                        <div class="alert alert-danger">
+                            {{ Session::get('error') }}
+                            {{ Session::put('error', nullS) }}
+                        </div>
+                    @endif
+                    <h3 class="mb-4 billing-heading">Billing Details</h3>
+                    <div class="row align-items-end">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="firstname">Firt Name</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="lastname">Last Name</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label for="country">State / Country</label>
+                                <div class="select-wrap">
+                                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                    <select name="" id="" class="form-control">
+                                        <option value="">France</option>
+                                        <option value="">Italy</option>
+                                        <option value="">Philippines</option>
+                                        <option value="">South Korea</option>
+                                        <option value="">Hongkong</option>
+                                        <option value="">Japan</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="streetaddress">Street Address</label>
+                                <input type="text" class="form-control" placeholder="House number and street name">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control"
+                                    placeholder="Appartment, suite, unit etc: (optional)">
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="towncity">Town / City</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="postcodezip">Postcode / ZIP *</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="phone">Phone</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="emailaddress">Email Address</label>
+                                <input type="text" class="form-control" placeholder="">
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-12">
+                            <div class="form-group mt-4">
+                                <div class="radio">
+                                    <label class="mr-3"><input type="radio" name="optradio"> Create an Account? </label>
+                                    <label><input type="radio" name="optradio"> Ship to different address</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {!! form::close() !!}
+                    <!-- END -->
+                </div>
+                <div class="col-xl-5">
+                    <div class="row mt-5 pt-3">
+                        <div class="col-md-12 d-flex mb-5">
+                            <div class="cart-detail cart-total p-3 p-md-4">
+                                <h3 class="billing-heading mb-4">Cart Total</h3>
+                                <p class="d-flex">
+                                    <span>Subtotal</span>
+                                    <span>$20.60</span>
+                                </p>
+                                <p class="d-flex">
+                                    <span>Delivery</span>
+                                    <span>$0.00</span>
+                                </p>
+                                <p class="d-flex">
+                                    <span>Discount</span>
+                                    <span>$3.00</span>
+                                </p>
+                                <hr>
+                                <p class="d-flex total-price">
+                                    <span>Total</span>
+                                    <span>${{ Session::get('cart')->totalPrice }}</span>
+                                </p>
+                            </div>
 
-							<h3 class="mb-4 billing-heading">Billing Details</h3>
-	          	<div class="row align-items-end">
-	          		<div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="firstname">Firt Name</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-	              </div>
-	              <div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="lastname">Last Name</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-                </div>
-                <div class="w-100"></div>
-		            <div class="col-md-12">
-		            	<div class="form-group">
-		            		<label for="country">State / Country</label>
-		            		<div class="select-wrap">
-		                  <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-		                  <select name="" id="" class="form-control">
-		                  	<option value="">France</option>
-		                    <option value="">Italy</option>
-		                    <option value="">Philippines</option>
-		                    <option value="">South Korea</option>
-		                    <option value="">Hongkong</option>
-		                    <option value="">Japan</option>
-		                  </select>
-		                </div>
-		            	</div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                	<label for="streetaddress">Street Address</label>
-	                  <input type="text" class="form-control" placeholder="House number and street name">
-	                </div>
-		            </div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                  <input type="text" class="form-control" placeholder="Appartment, suite, unit etc: (optional)">
-	                </div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-	                	<label for="towncity">Town / City</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-		            </div>
-		            <div class="col-md-6">
-		            	<div class="form-group">
-		            		<label for="postcodezip">Postcode / ZIP *</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-		            </div>
-		            <div class="w-100"></div>
-		            <div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="phone">Phone</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-	              </div>
-	              <div class="col-md-6">
-	                <div class="form-group">
-	                	<label for="emailaddress">Email Address</label>
-	                  <input type="text" class="form-control" placeholder="">
-	                </div>
-                </div>
-                <div class="w-100"></div>
-                <div class="col-md-12">
-                	<div class="form-group mt-4">
-										<div class="radio">
-										  <label class="mr-3"><input type="radio" name="optradio"> Create an Account? </label>
-										  <label><input type="radio" name="optradio"> Ship to different address</label>
-										</div>
-									</div>
-                </div>
-	            </div>
-	          {!!form::close()!!} <!-- END -->
-					</div>
-					<div class="col-xl-5">
-	          <div class="row mt-5 pt-3">
-	          	<div class="col-md-12 d-flex mb-5">
-	          		<div class="cart-detail cart-total p-3 p-md-4">
-	          			<h3 class="billing-heading mb-4">Cart Total</h3>
-	          			<p class="d-flex">
-		    						<span>Subtotal</span>
-		    						<span>$20.60</span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Delivery</span>
-		    						<span>$0.00</span>
-		    					</p>
-		    					<p class="d-flex">
-		    						<span>Discount</span>
-		    						<span>$3.00</span>
-		    					</p>
-		    					<hr>
-		    					<p class="d-flex total-price">
-		    						<span>Total</span>
-		    						<span>${{Session::get('cart')->totalprice}}</span>
-		    					</p>
-								</div>
-	          	
-	          	</div>
-	          </div>
-          </div> <!-- .col-md-8 -->
+                        </div>
+                    </div>
+                </div> <!-- .col-md-8 -->
+            </div>
         </div>
-      </div>
     </section> <!-- .section -->
 @endsection
 
 @section('scripts')
+    <script src="https://js.stripe.com/v2/"></script>
+    <script src="src/js/checkout.js"></script>
+    <script>
+        $(document).ready(function() {
 
-  <script>
-		$(document).ready(function(){
+            var quantitiy = 0;
+            $('.quantity-right-plus').click(function(e) {
 
-		var quantitiy=0;
-		   $('.quantity-right-plus').click(function(e){
-		        
-		        // Stop acting like a button
-		        e.preventDefault();
-		        // Get the field name
-		        var quantity = parseInt($('#quantity').val());
-		        
-		        // If is not undefined
-		            
-		            $('#quantity').val(quantity + 1);
+                // Stop acting like a button
+                e.preventDefault();
+                // Get the field name
+                var quantity = parseInt($('#quantity').val());
 
-		          
-		            // Increment
-		        
-		    });
+                // If is not undefined
 
-		     $('.quantity-left-minus').click(function(e){
-		        // Stop acting like a button
-		        e.preventDefault();
-		        // Get the field name
-		        var quantity = parseInt($('#quantity').val());
-		        
-		        // If is not undefined
-		      
-		            // Increment
-		            if(quantity>0){
-		            $('#quantity').val(quantity - 1);
-		            }
-		    });
-		    
-		});
-	</script>
+                $('#quantity').val(quantity + 1);
+
+
+                // Increment
+
+            });
+
+            $('.quantity-left-minus').click(function(e) {
+                // Stop acting like a button
+                e.preventDefault();
+                // Get the field name
+                var quantity = parseInt($('#quantity').val());
+
+                // If is not undefined
+
+                // Increment
+                if (quantity > 0) {
+                    $('#quantity').val(quantity - 1);
+                }
+            });
+
+        });
+    </script>
 
 @endsection
-    
