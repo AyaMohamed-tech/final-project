b24767a7b3cfaea8e61c76426c0eb4aa491cf8b9
alaa <eng,alaaebasyony@gmail.com>
2021-08-21 00:51:48 +0200

    1

--- resources/views/client/cart.blade.php
+++ resources/views/client/cart.blade.php
@@ -144,7 +144,7 @@
                         <hr>
                         <p class="d-flex total-price">
                             <span>Total</span>
-                            <span>${{ Session::get('cart')->totalPrice }}</span>
+                            <span>80$</span>
                         </p>
                     </div>
                     <p><a href="/checkout" class="btn btn-primary py-3 px-4">Proceed to Checkout</a></p>
