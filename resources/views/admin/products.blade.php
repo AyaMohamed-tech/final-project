21972e9bcd5b548a30f4596cd15e5e32c3f9d461
Aya mohammed <aya592648@gmail.com>
2021-08-27 19:05:17 +0200

    update

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -9,6 +9,11 @@
     <div class="card">
         <div class="card-body">
             <h4 class="card-title">Products</h4>
+            @if(Session::has('status'))
+                      <div class="alert alert-success">
+                            {{Session::get('status')}}
+                      </div>
+                  @endif
             <div class="row">
                 <div class="col-12">
                     <div class="table-responsive">
