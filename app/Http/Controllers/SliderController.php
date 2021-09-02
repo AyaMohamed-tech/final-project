cd908850526b18a53c40c79c8f9091f1189d1cb0
Aya mohammed <aya592648@gmail.com>
2021-09-02 22:16:52 +0200

    --

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -146,13 +146,10 @@ class SliderController extends Controller
 
         $slider->delete();
 
-<<<<<<< HEAD
-       
+    
        return redirect('/admin/sliders')->with('status','The Slider has been deleted successfully');
-=======
->>>>>>> fdf5d0ae0a1b2fe9eabcbc10284ccf168bf0a15e
 
-        return redirect('/sliders')->with('status', 'The Slider has been deleted successfully');
+       
     }
 
     public function unactivate_slider($id)
@@ -168,11 +165,8 @@ class SliderController extends Controller
 
         $slider->update();
 
-<<<<<<< HEAD
         return redirect('/admin/sliders')->with('status','The Slider status has been unactivated successfully');
-=======
-        return redirect('/sliders')->with('status', 'The Slider status has been unactivated successfully');
->>>>>>> fdf5d0ae0a1b2fe9eabcbc10284ccf168bf0a15e
+       
     }
     public function activate_slider($id)
     {
@@ -187,10 +181,7 @@ class SliderController extends Controller
 
         $slider->update();
 
-<<<<<<< HEAD
         return redirect('/admin/sliders')->with('status','The Slider status has been activated successfully');
-=======
-        return redirect('/sliders')->with('status', 'The Slider status has been activated successfully');
->>>>>>> fdf5d0ae0a1b2fe9eabcbc10284ccf168bf0a15e
+      
     }
 }
