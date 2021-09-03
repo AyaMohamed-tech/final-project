56695893ad5a397f95def6a3af3bf48531377b6d
alaa <eng,alaaebasyony@gmail.com>
2021-09-03 08:01:46 +0200

    adding search

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -26,7 +26,19 @@
 			</ul>
 
 
+
 		</div>
 	</div>
+
+
+
+</nav>
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">Find your product</a>
+  <form class="form-inline" action ="/search">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name ="query">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
 </nav>
 <!-- END nav -->
